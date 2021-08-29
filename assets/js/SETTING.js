11e14220e8600e807a2022f9dbc5de6142570a9b
Hiro <68377394+Hiro229@users.noreply.github.com>
2021-08-29 13:49:39 +0900

    Update SETTING.js

--- assets/js/SETTING.js
+++ assets/js/SETTING.js
@@ -1,6 +1,6 @@
 
 //お店のアドレス
-shopwalletaddress = "0x131231C27A749991270805Ae48754FABB2466d7F";
+shopwalletaddress = "0xbc1aFE196cb571Bf972AfaBBc2D3F0aDac4c15DA";
 
 //お値段(JPYC)
 pricing = 500;
@@ -11,5 +11,5 @@ image = "https://3.bp.blogspot.com/-9GGxkWC2bqM/UYns8kr1stI/AAAAAAAARbw/feLZrPmV
 
 
 //JPYC address
-jpyc_on_matic = "0x6AE7Dfc73E0dDE2aa99ac063DcF7e8A63265108c";
-
+//jpyc_on_matic = "0x6AE7Dfc73E0dDE2aa99ac063DcF7e8A63265108c";
+jpyc_on_rinkeby = "0xbD9c419003A36F187DAf1273FCe184e1341362C0";
